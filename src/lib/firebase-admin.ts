ffcd0f8e98b76cb153487621ee5120da3303d4ed
Rohith Bellamkonda <rohithbellamkonda0@gmail.com>
2025-07-11 07:28:25 +0000

    see remove the all the files and all the things you are build i want to

--- src/lib/firebase-admin.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is no longer needed with the new user profile creation flow
-// and can be safely ignored or deleted.
-// We are keeping it to avoid breaking any potential imports, but its functions are not used.
