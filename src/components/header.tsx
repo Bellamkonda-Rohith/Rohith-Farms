baf560729e6220d97c18df83572b477e0918b9cd
Rohith Bellamkonda <rohithbellamkonda0@gmail.com>
2025-07-11 09:51:34 +0000

    see after i login  in the application its showing admin login what its t

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -70,7 +70,7 @@ export function Header() {
        <Button asChild variant="outline" size="sm">
           <Link href="/admin/login">
               <Shield className="mr-2 h-4 w-4"/>
-              Login
+              Admin Login
           </Link>
       </Button>
     );
